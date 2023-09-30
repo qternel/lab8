eaff72ee92425ac08750a57006346e2913710531
Михаил Сидорченко <qternel@gmail.com>
2023-09-30 10:32:09 +0300

    add time.pas

---
+++ time.pas
@@ -0,0 +1,11 @@
+﻿begin
+  var year := ReadInteger('Введите текущий год:');
+  Assert(year >= 0);
+  
+  var isLeapYear := True;
+  
+  if ((year mod 100 = 0) and (year mod 400 <> 0)) or (year mod 4 <> 0) then
+    isLeapYear := False;
+  
+  Println($'Введённый год високосный: {isLeapYear}');
+end.
