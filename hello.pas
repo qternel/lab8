5865f59300e9ffbe51b01db6b5f4e4df47b9d513
Михаил Сидорченко <qternel@gmail.com>
2023-09-30 10:24:50 +0300

    update hello.pas

--- hello.pas
+++ hello.pas
@@ -1,3 +1,13 @@
 ﻿begin
-  Println('Привет, мир!');
+  var time := ReadInteger('Который час?');
+  Assert((time >= 0) and (time <= 23));
+  
+  case time of
+    4..10: Println('Доброе утро, мир!');
+    11..16: Println('Добрый день, мир!');
+    17..22: Println('Добрый вечер, мир!');
+    else
+      Println('Доброй ночи, мир!');
+  end;
+  
 end.
