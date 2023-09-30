fd8b34ae950dcd34cbeb918027384f651a44fd67
Михаил Сидорченко <qternel@gmail.com>
2023-09-30 10:45:04 +0300

    update time.pas, add timer.pas

--- time.pas
+++ time.pas
@@ -1,11 +1,11 @@
 ﻿begin
   var year := ReadInteger('Введите текущий год:');
-  Assert(year >= 0);
+  Assert(year > 0);
   
   var isLeapYear := True;
   
   if ((year mod 100 = 0) and (year mod 400 <> 0)) or (year mod 4 <> 0) then
     isLeapYear := False;
   
-  Println($'Введённый год високосный: {isLeapYear}');
+  Println((isLeapYear) ? 'Кол-во дней в году: 366' : 'Кол-во дней в году: 365');
 end.
---
+++ timer.pas
@@ -0,0 +1,17 @@
+﻿begin
+  var (hours, minutes, seconds) := ReadInteger3('Введите кол-во часов, минут, секунд:');
+  Assert((hours >= 0) and (minutes >= 0) and (seconds >= 0));
+  if seconds >= 60 then
+  begin
+    minutes += seconds div 60;
+    seconds := seconds mod 60;
+  end;
+  
+  if minutes >= 60 then
+  begin
+    hours += minutes div 60;
+    hours := minutes mod 60;
+  end;
+  
+  Println($'Общее количество секунд: {seconds + minutes * 60 + hours * 3600}');
+end.
